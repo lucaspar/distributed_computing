2e28476f83536a3b556cc5bb0e448d893bc78e16
Lucas <lucasparza@gmail.com>
2018-05-17 07:43:37 -0300

    hosts script

---
+++ glusterfs/hosts.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#################################################
+# Dynamically add or remove hosts from /etc/hosts
+#################################################
+
+if [ $# -ne 3 ]; then
+    echo -e "\n\tUSAGE:\t./hosts.sh <add|remove> <HOSTNAME> <IP>\n"
+    exit 1
+fi
+
+# Path to your hosts file
+hostsFile="/etc/hosts"
+
+# Hostname to add/remove.
+hostname=$2
+
+# IP address for host
+ip=$3
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
+remove() {
+    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
+        echo "$hostname found in $hostsFile. Removing now...";
+        try sudo sed -ie "/[[:space:]]$hostname/d" "$hostsFile";
+    else
+        yell "$hostname was not found in $hostsFile";
+    fi
+}
+
+add() {
+    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
+        yell "$hostname, already exists: $(grep $hostname $hostsFile)";
+    else
+        echo "Adding $hostname to $hostsFile...";
+        try printf "%s\t%s\n" "$ip" "$hostname" | sudo tee -a "$hostsFile" > /dev/null;
+
+        if [ -n "$(grep $hostname /etc/hosts)" ]; then
+            echo "$hostname was added succesfully:";
+            echo "$(grep $hostname /etc/hosts)";
+        else
+            die "Failed to add $hostname";
+        fi
+    fi
+}
+
+$@
